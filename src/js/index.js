d9ebd7155554ad8b20a741b6c481e67dd587ff03
mazey <mazey@mazey.net>
2018-06-04 14:33:59 +0800

    转连接线函数

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,40 @@
 // 渲染表格
 export const mRenderTable = (tbID = null, data = [], property = []) => {
-    const TBODY = document.querySelector(`#${tbID} tbody`)
-    const mNullToNA = str => {
-        return str === null ? 'N.A.' : str
+  const TBODY = document.querySelector(`#${tbID} tbody`)
+  const mNullToNA = str => {
+    return str === null ? 'N.A.' : str
+  }
+  let content = ''
+  TBODY.innerHTML = ''
+  // 无数据
+  if (!data.length) {
+    content = `<tr><td colspan="${property.length}">无数据</td></tr>`
+  } else {
+    for (let [i, max] = [0, data.length]; i < max; ++i) {
+      let item = data[i]
+      content += `<tr>`
+      for (let [i, max] = [0, property.length]; i < max; ++i) {
+        content += `<td>${mNullToNA(item[property[i]])}</td>`
+      }
+      content += `</tr>`
     }
-    let content = ''
-    TBODY.innerHTML = ''
-    // 无数据
-    if (!data.length) {
-        content = `<tr><td colspan="${property.length}">无数据</td></tr>`
-    } else {
-        for (let [i, max] = [0, data.length]; i < max; ++i) {
-            let item = data[i]
-            content += `<tr>`
-            for (let [i, max] = [0, property.length]; i < max; ++i) {
-                content += `<td>${mNullToNA(item[property[i]])}</td>`
-            }
-            content += `</tr>`
-        }
-    }
-    TBODY.innerHTML = content
+  }
+  TBODY.innerHTML = content
 }
 
 // 获取地址栏参数
 export const getQueryParam = param => {
-    let hashs = window.location.hash.split('?')
-    if (hashs.length === 1) {
-        return null
-    }
-    let reg = new RegExp(`(^|&)${param}=([^&]*)(&|$)`)
-    let ret = hashs[1].match(reg)
-    return ret ? ret[2] : null
+  let hashs = window.location.hash.split('?')
+  if (hashs.length === 1) {
+    return null
+  }
+  let reg = new RegExp(`(^|&)${param}=([^&]*)(&|$)`)
+  let ret = hashs[1].match(reg)
+  return ret ? ret[2] : null
+}
+
+// 驼峰转连接线
+export const camelCaseToKebabCase = camelCase => {
+  let kebabCase = camelCase.replace(/([A-Z])/g, '-$1').toLowerCase()
+  return kebabCase[0] === '-' ? kebabCase.substr(1) : kebabCase
 }
