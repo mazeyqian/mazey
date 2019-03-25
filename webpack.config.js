79ba08f7c1bca24fc7365f8ba33af0ae2b4bf415
钱程 <qiancheng@qutoutiao.net>
2019-03-25 20:26:41 +0800

    chore: webpack 配置。

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,10 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    rem: './src/rem.js',
-    app: './src/index.js',
-    h5: './src/h5.js'
+    index: './src/index.js',
   },
   output: {
-    filename: '[name].[hash:8].v1.0.0.js',
-    // filename: '[name].v1.0.0.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -26,20 +23,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      filename: path.resolve(__dirname, 'dist/pay.html'),
-      template: path.resolve(__dirname, 'src/pay.html'),
-      inject: true,
-      minify: {
-        // removeComments: true,
-        // collapseWhitespace: true,
-        // removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
-    }),
     new CleanWebpackPlugin(['dist'])
   ]
 };
