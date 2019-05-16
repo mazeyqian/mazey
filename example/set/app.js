41c11845e6f7abef8aab5b2ff269ca694cdecc20
钱程 <qiancheng@qutoutiao.net>
2019-05-16 17:22:51 +0800

    chore: 去除 example。

--- example/set/app.js
+++
@@ -1,3 +0,0 @@
-// import {mDataStructure} from '../../src/js/data-structure/set';
-// let s0 = new mDataStructure.MSet();
-import {tt} from './t.js'
--- example/set/t.js
+++
@@ -1,3 +0,0 @@
-export const tt = {
-  name: '123'
-}
