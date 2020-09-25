74ae608f82f861b101bb581e3319d7ffcd012d5a
钱程 <qiancheng@qutoutiao.net>
2020-09-26 00:01:35 +0800

    feat: 格式化 var

--- src/index.ts
+++ src/index.ts
@@ -826,10 +826,10 @@ export function setCookie(name: string, value: string, days: number, domain: str
  * @description 获取 Cookie
  */
 export function getCookie (name: string) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
     while (c.charAt(0) == ' ') {
       c = c.substring(1, c.length);
     }
