562bb093d7a511a9d88353ccdd36350b39197113
社区第六代前端 <mazeyqian@lilith.com>
2021-11-15 18:54:08 +0800

    feat: 添加图片样式和案例

--- src/test.ts
+++ src/test.ts
@@ -14,6 +14,7 @@ import {
   windowLoaded,
   isNumber,
   addInlineStyle,
+  floatToPercent,
   // getPerformance,
  } from './index';
 
@@ -24,7 +25,7 @@ console.log('数字测试：', isNumber(Infinity, { isUnFiniteAsNumber: true }))
 addInlineStyle({
   inlineStyle: `
     body {
-      background-color: #333;
+      background-color: #;
     }
   `,
   id: 'test',
@@ -37,13 +38,13 @@ addInlineStyle({
 // </style>
 
 // 添加无 id 的内联样式，重复添加会新增内联样式
-addInlineStyle({
-  inlineStyle: `
-    body {
-      background-color: #444;
-    }
-  `,
-});
+// addInlineStyle({
+//   inlineStyle: `
+//     body {
+//       background-color: #444;
+//     }
+//   `,
+// });
 
 // <style>
 //   body {
@@ -52,3 +53,45 @@ addInlineStyle({
 // </style>
 
 (window as any).addInlineStyle = addInlineStyle;
+
+// 获取图片距离顶部的距离
+function getBox2To () {
+  const translates = window.getComputedStyle(document.querySelector('.i') as any, null).transform;
+  const tanslateY = parseFloat(translates.substring(6).split(',')[5]);
+  return tanslateY;
+}
+console.log('获取图片距离顶部的距离', getBox2To());
+// 获取图片的高度
+function getImgHeight () {
+  return document.querySelector('.img')?.clientHeight;
+}
+console.log('获取图片的高度', getImgHeight());
+// 获取窗口的高度
+function getWindowHeight () {
+  return window.outerHeight;
+}
+console.log('获取窗口的高度', getWindowHeight());
+// 获取顶部溢出的百分比
+function getTopOverflowPercent () {
+  let box2To = getBox2To();
+  if (box2To >= 0) {
+    box2To = 0;
+  }
+  box2To = Math.abs(box2To);
+  const windowHeight = getWindowHeight();
+  const per = floatToPercent(box2To/windowHeight, 2);
+  return {
+    box2To,
+    per,
+  };
+}
+console.log('获取顶部溢出的百分比', getTopOverflowPercent());
+// 获取底部溢出的百分比
+function getBottomOverflowPercent () {
+  // 窗口高度
+  const windowHeight = getWindowHeight();
+  // 图片高度
+  const imgHeight = getImgHeight();
+  // 上面溢出的高度
+  const box2To = getBox2To();
+}
