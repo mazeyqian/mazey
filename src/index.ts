9ecefa052812658b17c3dd5f95aa5823cb6f98a1
社区第六代前端 <mazeyqian@lilith.com>
2021-11-16 18:32:56 +0800

    feat: 添加方法customScrollBarForTransformEle

--- src/index.ts
+++ src/index.ts
@@ -1408,3 +1408,154 @@ export function addInlineStyle({ inlineStyle = '', id = '' } = {}): boolean {
   }
   return true;
 }
+
+/**
+ * @method customScrollBarForTransformEle
+ * @description 为进行变换（transform）的元素设置滚动条
+ * @param {String} containerClassName 进行变换（transform）的元素的容器的类名
+ * @param {String} imgBoxClassName 图片的容器的类名
+ * @param {String} imgClassName 图片的类名
+ * @param {Object} imgDom 图片 Dom 对象
+ * @return {Boolean} 设置成功
+*/
+export function customScrollBarForTransformEle({ containerClassName = '', imgBoxClassName = '', imgClassName = '', imgDom = null } = {}): boolean {
+  // 变量
+  // const containerClassName = 'container';
+  // const imgBoxClassName = 'box';
+  // const imgClassName = 'img';
+  // const imgDom = document.querySelectorAll('.img')[0];
+  setTimeout(() => {
+    // 样式初始化
+    addInlineStyle({
+      inlineStyle: `
+        .${containerClassName}::after {
+          content: ' ';
+          position: fixed;
+          top: 0;
+          right: 5px;
+          min-height: 0vh;
+          max-height: 100vh;
+          width: 12px;
+          border-radius: 10px;
+          box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+          -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+          background-color: #555;
+        }
+      `,
+      id: 'bar-init'
+    });
+    const imgInstance = imgDom || document.querySelector(`.${imgClassName}`);
+    if (!imgInstance) {
+      console.warn('Need image\'s Dom instance.');
+      return false;
+    }
+    const Box2ToRes = getBox2To();
+    console.log('获取图片距离顶部的距离', Box2ToRes);
+    const ImgHeightRes = getImgHeight();
+    console.log('获取图片的高度', ImgHeightRes);
+    const WindowHeightRes = getWindowHeight();
+    console.log('获取窗口的高度', WindowHeightRes);
+    const TopOverflowPercentRes = getTopOverflowPercent();
+    console.log('获取顶部溢出的百分比', TopOverflowPercentRes);
+    const BottomOverflowPercentRes = getBottomOverflowPercent();
+    console.log('获取顶部溢出的百分比', BottomOverflowPercentRes);
+    const hideBarRes = hideBar();
+    console.log('如果都是 0 ，隐藏滚动条', hideBarRes);
+    if (!hideBarRes) {
+      const setBarTopRes = setBarTop();
+      console.log('设置顶部状态栏', setBarTopRes);
+      const setBarBottomRes = setBarBottom();
+      console.log('设置底部状态栏', setBarBottomRes);
+    }
+    // 获取图片距离顶部的距离
+    function getBox2To () {
+      const translates = window.getComputedStyle(document.querySelector(`.${imgBoxClassName}`) as any, null).transform;
+      const tanslateY = parseFloat(translates.substring(6).split(',')[5]);
+      return tanslateY;
+    }
+    // 获取图片的高度
+    function getImgHeight () {
+      return (imgInstance)?.clientHeight;
+    }
+    // 获取窗口的高度
+    function getWindowHeight () {
+      return (document.querySelector(`.${containerClassName}`) as any).clientHeight; // window.outerHeight;
+    }
+    // 获取顶部溢出的百分比
+    function getTopOverflowPercent () {
+      let box2To = Box2ToRes;
+      if (box2To >= 0) {
+        box2To = 0;
+      }
+      box2To = Math.abs(box2To);
+      // 图片高度
+      const imgHeight = ImgHeightRes;
+      const per = floatToPercent(box2To/(imgHeight as any), 2);
+      return {
+        box2To,
+        per,
+      };
+    }
+    // 获取底部溢出的百分比
+    function getBottomOverflowPercent () {
+      // 窗口高度
+      const windowHeight = WindowHeightRes;
+      // 图片高度
+      const imgHeight = ImgHeightRes;
+      // 上面溢出的高度
+      const box2To = Box2ToRes;
+      // 底部
+      const remindImageHeight = windowHeight - box2To;
+      let box2ToBottom = (imgHeight as any) - remindImageHeight;
+      if (box2ToBottom <= 0) {
+        box2ToBottom = 0;
+      }
+      const per = floatToPercent(box2ToBottom/(imgHeight as any), 2);
+      return {
+        box2ToBottom,
+        per,
+      };
+    }
+    // 如果都是 0 ，隐藏滚动条
+    function hideBar () {
+      const { box2To } = TopOverflowPercentRes;
+      const { box2ToBottom } = BottomOverflowPercentRes;
+      if (!box2To && !box2ToBottom) {
+        return addInlineStyle({
+          inlineStyle: `
+            .${containerClassName}::after {
+              display: none;
+            }
+          `,
+          id: 'bar-hide'
+        });
+      }
+      return false;
+    }
+    // 设置顶部状态栏
+    function setBarTop () {
+      const { per } = TopOverflowPercentRes;
+      return addInlineStyle({
+        inlineStyle: `
+          .${containerClassName}::after {
+            top: ${per};
+          }
+        `,
+        id: 'bar-top'
+      });
+    }
+    // 设置底部状态栏
+    function setBarBottom () {
+      const { per } = BottomOverflowPercentRes;
+      return addInlineStyle({
+        inlineStyle: `
+          .${containerClassName}::after {
+            bottom: ${per};
+          }
+        `,
+        id: 'bar-bottom'
+      });
+    }
+  }, 25);
+  return true;
+}
--- src/test.ts
+++ src/test.ts
@@ -15,6 +15,7 @@ import {
   isNumber,
   addInlineStyle,
   floatToPercent,
+  customScrollBarForTransformEle,
   // getPerformance,
  } from './index';
 
@@ -54,57 +55,143 @@ addInlineStyle({
 
 (window as any).addInlineStyle = addInlineStyle;
 
+customScrollBarForTransformEle({
+  containerClassName: 'container',
+  imgBoxClassName: 'box',
+  imgClassName: 'img1',
+  imgDom: null,
+});
+
 setTimeout(() => {
+// 变量
+const containerClassName = 'container';
+const imgBoxClassName = 'box';
+const imgClassName = 'img';
+const imgDom = document.querySelectorAll('.img')[0];
+// 样式初始化
+addInlineStyle({
+  inlineStyle: `
+    .${containerClassName}::after {
+      content: ' ';
+      position: fixed;
+      top: 0;
+      right: 5px;
+      min-height: 0vh;
+      max-height: 100vh;
+      width: 12px;
+      border-radius: 10px;
+      box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+      -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+      background-color: #555;
+    }
+  `,
+  id: 'bar-init'
+});
 // 获取图片距离顶部的距离
 function getBox2To () {
-  const translates = window.getComputedStyle(document.querySelector('.i') as any, null).transform;
+  const translates = window.getComputedStyle(document.querySelector(`.${imgBoxClassName}`) as any, null).transform;
   const tanslateY = parseFloat(translates.substring(6).split(',')[5]);
   return tanslateY;
 }
-console.log('获取图片距离顶部的距离', getBox2To());
+const Box2ToRes = getBox2To();
+console.log('获取图片距离顶部的距离', Box2ToRes);
 // 获取图片的高度
 function getImgHeight () {
-  return document.querySelector('.img')?.clientHeight;
+  return (imgDom || document.querySelector(`.${imgClassName}`))?.clientHeight;
 }
-console.log('获取图片的高度', getImgHeight());
+const ImgHeightRes = getImgHeight();
+console.log('获取图片的高度', ImgHeightRes);
 // 获取窗口的高度
 function getWindowHeight () {
-  return window.outerHeight;
+  return (document.querySelector(`.${containerClassName}`) as any).clientHeight; // window.outerHeight;
 }
-console.log('获取窗口的高度', getWindowHeight());
+const WindowHeightRes = getWindowHeight();
+console.log('获取窗口的高度', WindowHeightRes);
 // 获取顶部溢出的百分比
 function getTopOverflowPercent () {
-  let box2To = getBox2To();
+  let box2To = Box2ToRes;
   if (box2To >= 0) {
     box2To = 0;
   }
   box2To = Math.abs(box2To);
-  // const windowHeight = getWindowHeight();
   // 图片高度
-  const imgHeight = getImgHeight();
+  const imgHeight = ImgHeightRes;
   const per = floatToPercent(box2To/(imgHeight as any), 2);
   return {
     box2To,
     per,
   };
 }
-console.log('获取顶部溢出的百分比', getTopOverflowPercent());
+const TopOverflowPercentRes = getTopOverflowPercent();
+console.log('获取顶部溢出的百分比', TopOverflowPercentRes);
 // 获取底部溢出的百分比
 function getBottomOverflowPercent () {
   // 窗口高度
-  const windowHeight = getWindowHeight();
+  const windowHeight = WindowHeightRes;
   // 图片高度
-  const imgHeight = getImgHeight();
+  const imgHeight = ImgHeightRes;
   // 上面溢出的高度
-  const box2To = getBox2To();
-  // 底部(imgHeight as any)
+  const box2To = Box2ToRes;
+  // 底部
   const remindImageHeight = windowHeight - box2To;
-  const box2ToBottom = (imgHeight as any) - remindImageHeight;
+  let box2ToBottom = (imgHeight as any) - remindImageHeight;
+  if (box2ToBottom <= 0) {
+    box2ToBottom = 0;
+  }
   const per = floatToPercent(box2ToBottom/(imgHeight as any), 2);
   return {
     box2ToBottom,
     per,
   };
 }
-console.log('获取顶部溢出的百分比', getBottomOverflowPercent());
-}, 1000);
+const BottomOverflowPercentRes = getBottomOverflowPercent();
+console.log('获取顶部溢出的百分比', BottomOverflowPercentRes);
+// 如果都是 0 ，隐藏滚动条
+function hideBar () {
+  const { box2To } = TopOverflowPercentRes;
+  const { box2ToBottom } = BottomOverflowPercentRes;
+  if (!box2To && !box2ToBottom) {
+    return addInlineStyle({
+      inlineStyle: `
+        .${containerClassName}::after {
+          display: none;
+        }
+      `,
+      id: 'bar-hide'
+    });
+  }
+  return false;
+}
+const hideBarRes = hideBar();
+console.log('如果都是 0 ，隐藏滚动条', hideBarRes);
+// 设置顶部状态栏
+function setBarTop () {
+  const { box2To, per } = TopOverflowPercentRes;
+  return addInlineStyle({
+    inlineStyle: `
+      .${containerClassName}::after {
+        top: ${per};
+      }
+    `,
+    id: 'bar-top'
+  });
+}
+// 设置底部状态栏
+function setBarBottom () {
+  const { box2ToBottom, per } = BottomOverflowPercentRes;
+  return addInlineStyle({
+    inlineStyle: `
+      .${containerClassName}::after {
+        bottom: ${per};
+      }
+    `,
+    id: 'bar-bottom'
+  });
+}
+if (!hideBarRes) {
+  const setBarTopRes = setBarTop();
+  console.log('设置顶部状态栏', setBarTopRes);
+  const setBarBottomRes = setBarBottom();
+  console.log('设置底部状态栏', setBarBottomRes);
+}
+}, 10000000000000);
