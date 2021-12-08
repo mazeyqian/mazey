199d1c522d2124f5fd442fa86c1072192e309f95
钱程 <qiancheng@qutoutiao.net>
2021-12-08 20:29:59 +0800

    feat: 添加可定制的打印

--- src/test.ts
+++ src/test.ts
@@ -24,3 +24,14 @@ import {
 console.log('数字测试：', isNumber(Infinity, { isUnFiniteAsNumber: true }));
 
 console.log('计算适配屏幕宽度的图片尺寸和位置：', JSON.stringify(calcContainImageSizeAndPosition({ oriImageWidth: 300, oriImageHeight: 300, viewportWidth: 375, viewportHeight: 812 })));
+
+const methods = ['warn', 'error', 'info'];
+
+window.$QttGame = {};
+window.$QttGame.console = {};
+
+methods.forEach(method => {
+  window.$QttGame.console[method] = function (...argu) {
+    console[method](...argu)
+  }
+});
