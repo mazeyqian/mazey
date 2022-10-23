23a88f66e9af867bd8135e3a51e8eb4cfc07a36b
Mazey/后除 <mazey@mazey.net>
2022-10-23 17:54:36 +0800

    feat: add ready functions

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 /**
  * @author: Mazey Chu
  */
+
+// Define event.
+let defineListeners = {};
+
+// Add event.
+function addEvent(type, fn) {
+  if (typeof defineListeners[type] === "undefined") {
+      defineListeners[type] = [];
+  }
+  if (typeof fn === "function") {
+      defineListeners[type].push(fn);
+  }
+}
+
+// Invoke event.
+function fireEvent(type) {
+  let arrayEvent = defineListeners[type];
+  if (arrayEvent instanceof Array) {
+      for (let i = 0, length = arrayEvent.length; i < length; i++) {
+          if (typeof arrayEvent[i] === "function") {
+              arrayEvent[i]({
+                  type: type
+              });
+          }
+      }
+  }
+}
+
+// Remove event.
+function removeEvent(type, fn) {
+  let arrayEvent = defineListeners[type];
+  if (typeof type === "string" && arrayEvent instanceof Array) {
+      if (typeof fn === "function") {
+          for (let i = 0, length = arrayEvent.length; i < length; i++) {
+              if (arrayEvent[i] === fn) {
+                  defineListeners[type].splice(i, 1);
+                  break;
+              }
+          }
+      } else {
+          delete defineListeners[type];
+      }
+  }
+}
