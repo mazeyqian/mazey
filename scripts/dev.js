da797a7e737447d0ebf858091168fe4f77211e37
Mazey Chu <mazey@mazey.net>
2022-11-18 19:27:34 +0800

    feat: optimize `Git Push`

--- scripts/dev.js
+++ scripts/dev.js
@@ -3,8 +3,17 @@
 const execa = require('execa');
 
 (async () => {
-  const { stdout: pushMsg } = await execa('git', ['push']);
-  console.log('pushMsg:', pushMsg);
-  const { stdout: currentBranch } = await execa('git', ['branch', '--show-current'], { stdio: 'pipe' });
-  console.log('currentBranch:', currentBranch); // currentBranch: fix/rollup_dependences_mon_oct_31st_2022
+  // const { stdout: pushMsg = '' } = await execa('git', ['push'], { stdio: 'pipe' })
+  //   .catch(error => {
+  //     console.log('error:', error.message);
+  //   });
+  try {
+    await execa('git', ['push'], { stdio: 'pipe' });
+  } catch (error) {
+    console.log('error:', error.message);
+    const { stdout: currentBranch } = await execa('git', ['branch', '--show-current'], { stdio: 'pipe' });
+    console.log('currentBranch:', currentBranch); // currentBranch: fix/rollup_dependences_mon_oct_31st_2022
+    await execa('git', ['push', '--set-upstream', 'origin', currentBranch]);
+  }
+  // console.log('pushMsg:', pushMsg);
 })();
