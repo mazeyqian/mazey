1d05093c2158e797706429792be7a5d406328310
钱程 <mazey@mazey.net>
2022-11-27 01:04:51 +0800

    feat: test dom correctly

--- test/dom.test.js
+++ test/dom.test.js
@@ -4,7 +4,7 @@
 /* eslint-disable no-undef */
 // DOM
 
-import { newLine, loadScript } from '../lib/index.esm';
+import { newLine, getDomain } from '../lib/index.esm';
 
 test(`newLine: Transfer 'a\nb\nc' to 'a<br />b<br />c'?`, () => {
   expect(newLine('a\nb\nc')).toBe('a<br />b<br />c');
@@ -14,14 +14,19 @@ test(`newLine: Transfer 'a\n\nbc' to 'a<br /><br />bc'?`, () => {
   expect(newLine('a\n\nbc')).toBe('a<br /><br />bc');
 });
 
-test(`Load JavsScript script: 'https://i.mazey.net/mazey/lib/mazey.min.js'?`, async () => {
-  const res = await loadScript(
-    'https://i.mazey.net/mazey/lib/mazey.min.js',
-    {
-      id: 'iamid', // (Optional) script ID, default none
-      timeout: 5000, // (Optional) timeout, default `5000`
-      isDefer: false, // (Optional) defer, default `false`
-    }
-  );
-  expect(res).toBe(true);
+// test(`Load JavsScript script: 'https://i.mazey.net/mazey/lib/mazey.min.js'?`, async () => {
+//   const res = await loadScript(
+//     'https://i.mazey.net/mazey/lib/mazey.min.js',
+//     {
+//       id: 'iamid', // (Optional) script ID, default none
+//       timeout: 5000, // (Optional) timeout, default `5000`
+//       isDefer: false, // (Optional) defer, default `false`
+//     }
+//   );
+//   expect(res).toBe(true);
+// });
+
+test(`Can get Domain's params correctly? 'https://i.mazey.net/mazey/lib/mazey.min.js'?`, async () => {
+  const res = await getDomain('http://example.com/test/thanks?t1=1&t2=2&t3=3&t4=4', ['hostname', 'pathname']); // example.com/test/thanks
+  expect(res).toBe('example.com/test/thanks');
 });
