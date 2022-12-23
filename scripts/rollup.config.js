c390eecd4ec1861034f4bc1e4112285b1a1d460d
Mazey Chu <mazey@mazey.net>
2022-12-23 11:23:51 +0800

    feat: clear scripts

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -9,13 +9,62 @@ import { terser } from 'rollup-plugin-terser';
 // import copy from 'rollup-plugin-copy';
 
 const { _resolve } = require('./build-helper');
-const pkgVersion = process.env.VERSION || require('../package.json').version;
+const pkgVersion = process.env.SCRIPTS_NPM_PACKAGE_VERSION || process.env.VERSION || require('../package.json').version;
+const debugMode = process.env.SCRIPTS_NPM_PACKAGE_DEBUG;
 const banner =
   '/*!\n' +
   ` * mazey v${pkgVersion}\n` +
   ` * (c) 2018-${new Date().getFullYear()} Mazey Chu\n` +
   ' * Released under the MIT License.\n' +
   ' */';
+
+const plugins = [
+  // Remove the `lib` directory before rebuilding.
+  // https://github.com/aMarCruz/rollup-plugin-cleanup
+  cleaner({
+    targets: [
+      _resolve('../lib/'),
+    ],
+  }),
+  rollupTypescript(),
+  commonjs({
+    include: /node_modules/,
+  }),
+  babel({
+    runtimeHelpers: true,
+    // 只转换源代码，不运行外部依赖
+    exclude: 'node_modules/**',
+    // babel 默认不支持 ts 需要手动添加
+    extensions: [
+      ...DEFAULT_EXTENSIONS,
+      '.ts',
+    ],
+  }),
+  // uglify(),
+  // https://www.npmjs.com/package/rollup-plugin-copy
+  // copy({
+  //   targets: [
+  //     {
+  //       src: _resolve('../src/t.html'),
+  //       dest: _resolve('../docs'),
+  //     },
+  //   ],
+  // }),
+];
+
+// console.log('debugMode:', debugMode);
+if (debugMode !== 'open') {
+  plugins.push(
+    // Add minification.
+    // https://github.com/TrySound/rollup-plugin-terser
+    terser({ // https://github.com/terser/terser
+      format: {
+        // https://github.com/terser/terser#format-options
+        comments: /^!\n\s\*\smazey/, // 'some', // `false` to omit comments in the output
+      },
+    }),
+  );
+}
 
 // https://rollupjs.org/guide/en/
 export default {
@@ -39,46 +88,6 @@ export default {
       banner,
     },
   ],
-  plugins: [
-    // Remove the `lib` directory before rebuilding.
-    // https://github.com/aMarCruz/rollup-plugin-cleanup
-    cleaner({
-      targets: [
-        _resolve('../lib/'),
-      ],
-    }),
-    rollupTypescript(),
-    commonjs({
-      include: /node_modules/,
-    }),
-    babel({
-      runtimeHelpers: true,
-      // 只转换源代码，不运行外部依赖
-      exclude: 'node_modules/**',
-      // babel 默认不支持 ts 需要手动添加
-      extensions: [
-        ...DEFAULT_EXTENSIONS,
-        '.ts',
-      ],
-    }),
-    // Add minification.
-    // https://github.com/TrySound/rollup-plugin-terser
-    terser({ // https://github.com/terser/terser
-      format: {
-        // https://github.com/terser/terser#format-options
-        comments: /^!\n\s\*\smazey/, // 'some', // `false` to omit comments in the output
-      },
-    }),
-    // uglify(),
-    // https://www.npmjs.com/package/rollup-plugin-copy
-    // copy({
-    //   targets: [
-    //     {
-    //       src: _resolve('../src/t.html'),
-    //       dest: _resolve('../docs'),
-    //     },
-    //   ],
-    // }),
-  ],
+  plugins,
   external: [],
 };
