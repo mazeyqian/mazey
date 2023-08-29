96e925bfb5b0f1f8f0d261404d8da3eea1f656b7
Mazey Chu <mazey@mazey.net>
2023-08-29 22:03:14 +0800

    test: `isValidHttpUrl`

--- src/index.ts
+++ src/index.ts
@@ -2921,6 +2921,27 @@ export function isValidUrl(url: string): boolean {
 }
 
 /**
+ * Check if the given string is a valid HTTP/HTTPS URL.
+ *
+ * @example
+ * ```js
+ * isValidHttpUrl('https://www.example.com'); // true
+ * isValidHttpUrl('http://example.com/path/exx/ss'); // true
+ * isValidHttpUrl('https://www.example.com/?q=hello&age=24#world'); // true
+ * isValidHttpUrl('http://www.example.com/#world?id=9'); // true
+ * isValidHttpUrl('ftp://example.com'); // false
+ * ```
+ *
+ * @param url
+ * @returns {boolean} Return true if the given string is a valid HTTP/HTTPS URL.
+ * @category URL
+ */
+export function isValidHttpUrl(url: string): boolean {
+  const reg = /^https?:\/\/[-a-zA-Z0-9@:%._+~#=]{1,256}\b([-a-zA-Z0-9\u4E00-\u9FA5()!@:%_+.~#?&//=]*)/;
+  return reg.test(url);
+}
+
+/**
  * Check if the given string is a mobile phone number.
  *
  * @example
