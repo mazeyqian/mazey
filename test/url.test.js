9e48dcf95a0a9c9ab82f6704297eca37c14c9c09
Cheng <mazeyqian@gmail.com>
2024-02-06 12:07:41 +0800

    fix: the wrong value in the hash situation
    Close #201

--- test/url.test.js
+++ test/url.test.js
@@ -129,35 +129,54 @@ test('Update query parameter in URL with hash fragment', () => {
   expect(updatedUrl).toBe('https://example.com/page?param1=updatedValue#section1');
 });
 
-// Test case 1: Single value parameter
-test('getUrlParam - Single value parameter', () => {
-  const url = 'https://example.com/?param1=value1&param2=value2';
-  const param = 'param1';
-  const result = getUrlParam(url, param);
-  expect(result).toBe('value1');
-});
 
-// Test case 2: Single value parameter
-test('getUrlParam - Array value parameter', () => {
-  const url = 'https://example.com/?param1=value1&param2=value2#path?param1=value1&param2=val2val';
-  const param = 'param1';
-  const result = getUrlParam(url, param);
-  expect(result).toEqual('value1');
-});
+describe('getUrlParam', () => {
+  // Test case 1: Single value parameter
+  test('Single value parameter', () => {
+    const url = 'https://example.com/?param1=value1&param2=value2';
+    const param = 'param1';
+    const result = getUrlParam(url, param);
+    expect(result).toBe('value1');
+  });
 
-// Test case 3: Return array option
-test('getUrlParam - Return array option', () => {
-  const url = 'https://example.com/?param1=value1&param1=value2';
-  const param = 'param1';
-  const options = { returnArray: true };
-  const result = getUrlParam(url, param, options);
-  expect(result).toEqual([ 'value1', 'value2' ]);
-});
+  // Test case 2: Single value parameter
+  test('Multiple value parameter', () => {
+    const url = 'https://example.com/?param1=value1&param2=value2#path?param1=value1&param2=val2val';
+    const param = 'param1';
+    const result = getUrlParam(url, param);
+    expect(result).toEqual('value1');
+  });
 
-// Test case 4: Non-existing parameter
-test('getUrlParam - Non-existing parameter', () => {
-  const url = 'https://example.com/?param1=value1&param2=value2';
-  const param = 'param3';
-  const result = getUrlParam(url, param);
-  expect(result).toBe('');
+  // Test case 3: Return array option
+  test('Return array option', () => {
+    const url = 'https://example.com/?param1=value1&param1=value2';
+    const param = 'param1';
+    const options = { returnArray: true };
+    const result = getUrlParam(url, param, options);
+    expect(result).toEqual([ 'value1', 'value2' ]);
+  });
+  // Empty
+  test('Return array option: Empty', () => {
+    const url = 'https://example.com/?param1=value1&param2=value2';
+    const param = 'value3';
+    const options = { returnArray: true };
+    const result = getUrlParam(url, param, options);
+    expect(result).toEqual([]);
+  });
+
+  // Test case 4: Non-existing parameter
+  test('Non-existing parameter', () => {
+    const url = 'https://example.com/?param1=value1&param2=value2';
+    const param = 'param3';
+    const result = getUrlParam(url, param);
+    expect(result).toBe(null);
+  });
+
+  // Get value when hash fragment is present
+  test('Get value when hash fragment is present', () => {
+    const url = 'https://example.com/?param1=value1&param2=value2#section1';
+    const param = 'param2';
+    const result = getUrlParam(url, param);
+    expect(result).toBe('value2');
+  });
 });
