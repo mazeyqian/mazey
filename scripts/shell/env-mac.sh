78cc56b433071fbb7f3610a1a78cf80d10e0e544
Cheng <mazeyqian@gmail.com>
2024-02-21 12:03:12 +0800

    chore: migrate shell scripts
    Remove Node Version and Build Scripts

--- scripts/shell/env-mac.sh
+++ scripts/shell/env-mac.sh
@@ -2,7 +2,5 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";
 
-. scripts/env.sh
-
-# Build
-npm run build:debug
+# Node Version
+nvm use 16.19.0
