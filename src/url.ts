5f10289c04ac61a7cd5c83c416b385b8dfbaeb59
Cheng <mazeyqian@gmail.com>
2024-02-29 11:55:36 +0800

    feat(url): get domain

--- src/url.ts
+++ src/url.ts
@@ -158,3 +158,47 @@ export function getHashQueryParam(param: string): string {
   const ret = hashs[1].match(reg);
   return ret ? ret[2] : '';
 }
+
+/**
+ * Get the domain of URL, and other params.
+ *
+ * Usage:
+ *
+ * ```javascript
+ * const ret1 = getDomain('http://example.com/?t1=1&t2=2&t3=3&t4=4');
+ * const ret2 = getDomain('http://example.com/test/thanks?t1=1&t2=2&t3=3&t4=4', ['hostname', 'pathname']);
+ * const ret3 = getDomain('http://example.com:7890/test/thanks', ['hostname']);
+ * const ret4 = getDomain('http://example.com:7890/test/thanks', ['host']); // With Port
+ * const ret5 = getDomain('http://example.com:7890/test/thanks', ['origin']);
+ * const ret6 = getDomain('http://example.com:7890/test/thanks?id=1', ['origin', 'pathname', 'search']);
+ * console.log(ret1);
+ * console.log(ret2);
+ * console.log(ret3);
+ * console.log(ret4);
+ * console.log(ret5);
+ * console.log(ret6);
+ * ```
+ *
+ * Output:
+ *
+ * ```text
+ * example.com
+ * example.com/test/thanks
+ * example.com
+ * example.com:7890
+ * http://example.com:7890
+ * http://example.com:7890/test/thanks?id=1
+ * ```
+ *
+ * @param {string} url
+ * @param {array} rules Object.keys(location), ['href', 'origin', 'protocol', 'host', 'hostname', 'port', 'pathname', 'search', 'hash'], ['hostname', 'pathname'] = 'km.mazey.net/plugins/servlet/mobile'
+ * @category URL
+ */
+export function getDomain(url: string, rules = [ 'hostname' ]): string {
+  const aEl: HTMLAnchorElement = document.createElement('a');
+  aEl.href = url;
+  return rules.reduce((ret, v) => {
+    ret += aEl[v as keyof HTMLAnchorElement];
+    return ret;
+  }, '');
+}
